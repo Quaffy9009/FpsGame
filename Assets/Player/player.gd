0031c122eabab2ee1a159d3e69a1c6763421647b
Afaz Gamedev <81065049+Afaz-Gamedev@users.noreply.github.com>
2022-02-25 13:16:57 +0800

    a

---
+++ Assets/Player/player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody
+
+#settings
+export var speed = 100
+export var gravity = 100
+export var JumpPower = 100
+export var AirResistance = 10
+
+#properties
+var JumpForce = 0
+var is_on_floor = false
+
+
+
+func _ready():
+	pass 
+
+
